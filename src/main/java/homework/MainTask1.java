9d2e23183c6a7559539c5c208a71c6869f4cfc1f
Nikita Novikov <monstars51@gmail.com>
2023-05-30 21:33:59 +0300

    50%

--- src/main/java/homework/MainTask1.java
+++ src/main/java/homework/MainTask1.java
@@ -1,19 +1,38 @@
 package homework;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainTask1 {
-    public static void main(String[] args) {
-        String result = generateRoute("RLRFR", 100);
-        char symbol = 'R';
-        int count = 0;
-        for (int i = 0; i < result.length(); i++) {
-            if (result.charAt(i) == symbol){
-                count++;
-            }
-        }
-        System.out.println("Самое частое количество повторений " + count);
+    public static final char SYMBOL = 'R';
+    //TODO Количество потоков равно количеству генерируемых маршрутов и равно 1000
+    public static final int ROUTE = 10;
 
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> list = new ArrayList<>();
+        String[] routes = new String[ROUTE];
+        for (int i = 0; i < ROUTE; i++) {
+            routes[i] = generateRoute("RLRFR", 100);
+        }
+        for (String route : routes) {
+            Runnable runnable = () -> {
+                int count = 0;
+                for (int j = 0; j < route.length(); j++) {
+                    if (route.charAt(j) == SYMBOL) {
+                        count++;
+                    }
+                }
+                System.out.println("Самое частое количество повторений " + count);
+            };
+            list.add(new Thread(runnable));
+        }
+        for (Thread thread : list) {
+            thread.start();
+        }
+        for (Thread thread : list) {
+            thread.join(); // зависаем, ждём когда поток, объект которого лежит в thread, завершится
+        }
     }
 
     public static String generateRoute(String letters, int length) {
