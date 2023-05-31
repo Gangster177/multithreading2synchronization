1187baa4b966b32fc46fb046656c327807d24fcd
Nikita Novikov <monstars51@gmail.com>
2023-05-31 13:15:31 +0300

    first task completed

--- src/main/java/homework/MainTask1.java
+++ src/main/java/homework/MainTask1.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 public class MainTask1 {
     public static final char SYMBOL = 'R';
-    //TODO Количество потоков равно количеству генерируемых маршрутов и равно 1000
-    public static final int ROUTE = 10;
+    public static final int ROUTE = 1000;
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
     public static void main(String[] args) throws InterruptedException {
@@ -16,7 +15,6 @@ public class MainTask1 {
         }
         for (String route : routes) {
             Runnable runnable = () -> {
-
                 int count = 0;
                 for (int j = 0; j < route.length(); j++) {
                     if (route.charAt(j) == SYMBOL) {
@@ -30,7 +28,6 @@ public class MainTask1 {
                         sizeToFreq.put(count, 1);
                     }
                 }
-                //System.out.println("Самое частое количество повторений " + count);
             };
             Thread thread = new Thread(runnable);
             thread.start();
@@ -39,18 +36,8 @@ public class MainTask1 {
         for (Thread thread : list) {
             thread.join();
         }
-        Map.Entry<Integer, Integer> maxEntry = sizeToFreq.entrySet().stream()
-                .max(Comparator.comparing(Map.Entry::getValue))
-                .orElse(null);
-        System.out.printf("Самое частое количество повторений %d (встретилось %d раз) \n", maxEntry.getKey(), maxEntry.getValue());
-        System.out.println("Другие размеры:");
-        if (!maxEntry.equals(sizeToFreq)) {
-            sizeToFreq.entrySet().stream()
-                    .sorted(Map.Entry.comparingByValue((a, b) -> b - a))
-                    .forEach(e -> System.out.printf("- %d (%d раз)\n", e.getKey(), e.getValue()));
-        }
+        printMessage();
     }
-
 
     public static String generateRoute(String letters, int length) {
         Random random = new Random();
@@ -59,5 +46,19 @@ public class MainTask1 {
             route.append(letters.charAt(random.nextInt(letters.length())));
         }
         return route.toString();
+    }
+
+    public static void printMessage() {
+        Map.Entry<Integer, Integer> maxEntry = sizeToFreq.entrySet().stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .orElse(null);
+        System.out.printf("Самое частое количество повторений %d (встретилось %d раз) \n", maxEntry.getKey(), maxEntry.getValue());
+        sizeToFreq.remove(maxEntry.getKey());
+        System.out.println("Другие размеры:");
+        if (!maxEntry.equals(sizeToFreq)) {
+            sizeToFreq.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue((a, b) -> b - a))
+                    .forEach(e -> System.out.printf("- %d (%d раз)\n", e.getKey(), e.getValue()));
+        }
     }
 }
